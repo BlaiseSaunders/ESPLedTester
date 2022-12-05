c6af3c4aa858709c061ecfd6146cf1f9e30b2bc2
Sky Velleity <velleity@outlook.com.au>
2022-12-06 00:01:04 +1100

    fix: implemented suggestions from review

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,14 @@
 #define DATA_PIN 1
 #define DATA_PIN_2 8
 
+
 // Define the array of leds
 CRGB leds[NUM_LEDS];
 
-int iterateSpeed = 500; // Delay in ms
+int iterateSpeed = 32; // Delay in ms
+
+int maxBright = 128;
+int minBright = 32;
 
 void setup() 
 { 
@@ -22,8 +26,9 @@ void setup()
 
 void lowPulse()
 {
-		//std::vector<int> codes = {CRGB::Red, CRGB::Green, CRGB::Blue, CRGB::AliceBlue, CRGB::White};
-	/*std::vector<int> codes = {CRGB::White};
+	/*
+	std::vector<int> codes = {CRGB::Red, CRGB::Green, CRGB::Blue, CRGB::AliceBlue, CRGB::White};
+	std::vector<int> codes = {CRGB::White};
 
 	// Iterate through colors, displaying with `iterateSpeed` delay
 	for (auto color : codes)
@@ -31,7 +36,8 @@ void lowPulse()
 		leds[0] = color;
 		FastLED.show();
 		delay(iterateSpeed);
-	}*/
+	}
+	*/
 
 	for (int j = 0; j < 255; j++)
 	{
@@ -54,20 +60,22 @@ void rainbowStrobe()
 			//The brighness will make multiple steps.
 			//There's no reason why the brighness can't change, for example,
 			//four times faster than the colour does, but I'm lazy so here we are
-			spot_brightness = (i+(j))%256;
-			if (spot_brightness > 128)
-				spot_brightness = 128 - (spot_brightness - 128);
-			if (spot_brightness < 32)
-				spot_brightness = 32;
+			spot_brightness = (i+(j))% (maxBright * 2);
+			if (spot_brightness > maxBright)
+				spot_brightness = maxBright - (spot_brightness - maxBright);
+			if (spot_brightness < minBright)
+				spot_brightness = minBright;
 			leds[i] = CHSV((i+(j/8))%256, 255, spot_brightness);			
 		}
 		FastLED.show();
-		delay(32);
+		delay(iterateSpeed);
 	}
 }
 
 
 void loop()
 {
+	//Uncomment the pattern you would like:
+	//lowPulse();
 	rainbowStrobe();
 }
